d9d1428f7c5d848ffd25f65b2c3b60d20f2b8655
Lukas Erni <lukaserni@Lukass-MacBook-Pro.local>
2014-04-03 23:04:35 +0200

    MultiFormPurgeTask as BuildTask
    DailyTask is deprecated in 3.1

--- code/tasks/MultiFormPurgeTask.php
+++ code/tasks/MultiFormPurgeTask.php
@@ -10,32 +10,22 @@
  * 
  * @package multiform
  */
-class MultiFormPurgeTask extends DailyTask {
-	
-	/**
-	 * Days after which sessions expire and
-	 * are automatically deleted.
-	 * 
-	 * @var int
-	 */
-	public static $session_expiry_days = 7;
 
-	/**
-	 * Run this cron task.
-	 * 
-	 * Go through all MultiFormSession records that
-	 * are older than the days specified in $session_expiry_days
-	 * and delete them.
-	 */
-	public function run() {
+
+class MultiFormPurgeTask extends BuildTask {
+    protected $title = "Delets Steps & Session from MultiForm"; // title of the script
+    protected $description = 'Go through all MultiFormSession records that are older than the days specified in $session_expiry_days and delete them.'; // description of what it does
+    public static $session_expiry_days = 5;
+    function run($request) { 
 		$sessions = $this->getExpiredSessions();
 		$delCount = 0;
 		if($sessions) foreach($sessions as $session) {
-			if($session->delete()) $delCount++;
+			// X if($session->delete()) $delCount++;
+			$session->delete();
+ 			$delCount++;
 		}
-		echo $delCount . ' session records deleted that were older than ' . self::$session_expiry_days . ' days.';
-	}
-
+        $this->debugMessage($delCount . ' session records deleted that were older than ' . self::$session_expiry_days . ' days.'); 
+    } 
 	/**
 	 * Return all MultiFormSession database records that are older than
 	 * the days specified in $session_expiry_days
@@ -48,5 +38,9 @@ class MultiFormPurgeTask extends DailyTask {
 			"DATEDIFF(NOW(), \"MultiFormSession\".\"Created\") > " . self::$session_expiry_days
 		);
 	}
-	
+    protected function debugMessage($msg) { 
+        if(!SapphireTest::is_running_test()) { 
+            Debug::message($msg); 
+        }
+    }
 }
